ac77cff416ebfe43c6b6dc7449378fcf6c41e75a
rahulsaha2005 <jived1224@gmail.com>
2026-01-03 22:02:27 +0530

    update url for deploy

--- frontened/src/utils/constant.js
+++ frontened/src/utils/constant.js
@@ -1,3 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const FRIEND_API_END_POINT="http://localhost:8000/api/v1/user/friend";
-
+export const USER_API_END_POINT =
+  "https://contactwebapplication.onrender.com/api/v1/user";
+export const FRIEND_API_END_POINT =
+  "https://contactwebapplication.onrender.com/api/v1/user/friend";
